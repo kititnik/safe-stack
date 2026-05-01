a544db64e634094b1b8ebf25762e3fde74ad4741
kititnik <kititnik@gmail.com>
2026-05-01 19:46:30 +0300

    Add hash for stack

--- src/stack.c
+++ src/stack.c
@@ -1,4 +1,5 @@
 #include "stack.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -16,7 +17,45 @@ struct Stack {
     size_t capacity;
     size_t size;
     type* data;
+
+    uint64_t hash;
+    uint64_t base;
+    uint64_t inv_base;
 };
+
+uint64_t generate_base() {
+    uint64_t base = ((uint64_t)rand() << 33) ^ ((uint64_t)rand() << 1) ^ 1ULL;
+    return base;
+}
+
+uint64_t compute_inv_base(uint64_t base) {
+    uint64_t inv_base = 1;
+    for(int i = 0; i < 6; i++) {
+        inv_base *= 2-base*inv_base;
+    }
+    return inv_base;
+}
+
+void push_update_hash(Stack* stack, type value) {
+    stack->hash = stack->hash * stack->base + (uint64_t)value;
+}
+
+void pop_update_hash(Stack* stack, type value) {
+    stack->hash = (stack->hash - (uint64_t)value) * stack->inv_base;
+}
+
+int check_integrity(Stack* stack, int* out) {
+    if(stack == NULL) {
+        fprintf(stderr, "%s", NULL_STACK_POINTER_ERROR);
+        return EXIT_FAILURE;
+    }
+    uint64_t hash = 0;
+    for(int i = 0; i < stack->size; i++) {
+        hash = hash * stack->base + (uint64_t)stack->data[i];
+    }
+    *out = (hash == stack->hash);
+    return EXIT_FAILURE;
+}
 
 Stack* init(size_t start_capacity) {
     if(start_capacity < 1) {
@@ -35,6 +74,8 @@ Stack* init(size_t start_capacity) {
         return NULL;
     }
     stack->size = 0;
+    stack->base = generate_base();
+    stack->inv_base = compute_inv_base(stack->base);
     return stack;
 }
 
@@ -68,6 +109,7 @@ int push(Stack* stack, type value) {
         }
     }
     stack->data[stack->size] = value;
+    push_update_hash(stack, value);
     stack->size++;
     return EXIT_SUCCESS;
 }
@@ -81,7 +123,10 @@ int pop(Stack* stack) {
         fprintf(stderr, "%s", EMPTY_STACK_POP_ERROR);
         return EXIT_FAILURE;
     }
+    type value = stack->data[stack->size-1];
+    pop_update_hash(stack, value);
     stack->size--;
+    
     return EXIT_SUCCESS;
 }
 
